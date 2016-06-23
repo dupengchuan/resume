c3d273fd13f152981800b44e6b42b2e042b32637
Kevin <826998892@qq.com>
2016-06-23 14:16:15 +0800

    modify

--- languages/en-US.js
+++ languages/en-US.js
@@ -8,15 +8,6 @@ module.exports = {
     "blog": "Blog",
     "github": "Github"
   },
-  skills_type: {
-    "language": "Programing Languages",
-    "structs": "Frameworks And Tools",
-    "other": "Other"
-  },
-  project_type: {
-    "personal": "Personal",
-    "company" : "Company project"
-  },
   section_names: {
     "information": {
       "en": "Information"
--- languages/zh-CN.js
+++ languages/zh-CN.js
@@ -1,21 +1,12 @@
 module.exports = {
   information_type: {
-    "name": "姓名",
+    "name": "姓&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp名",
     "position": "求职意向",
     "location": "所在地",
     "phone": "手机号码",
     "email": "电子邮箱",
-    "blog": "博客",
+    "blog": "技术博客",
     "github": "Github"
-  },
-  skills_type: {
-    "language": "掌握语言",
-    "structs": "框架工具",
-    "other": "其他"
-  },
-  project_type: {
-    "personal": "项目经验",
-    "company" : "公司项目"
   },
   section_names: {
     "information": {
